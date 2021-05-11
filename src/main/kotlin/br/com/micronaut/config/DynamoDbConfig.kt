1e30066cea90d67067edf5cda91b43ae2c09448b
Luiz Gustavo <32493929+luizgustv@users.noreply.github.com>
2021-05-10 21:48:48 -0300

    adding aws db configs

--- src/main/kotlin/br/com/micronaut/config/DynamoDbConfig.kt
+++ src/main/kotlin/br/com/micronaut/config/DynamoDbConfig.kt
@@ -1,5 +1,7 @@
 package br.com.micronaut.config
 
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import java.net.URI
@@ -8,9 +10,10 @@ class DynamoDbConfig {
 
     fun build(): DynamoDbClient =
         DynamoDbClient.builder()
+            .credentialsProvider(StaticCredentialsProvider.create(buildAwsCredentials()))
             .region(Region.of("\${aws.region}"))
             .endpointOverride(URI("\${aws.endpoint}"))
             .build()
 
-
+    private fun buildAwsCredentials() = AwsBasicCredentials.create("\${aws.accessKeyId}","\${aws.secretKey}")
 }
