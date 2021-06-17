c8c9d245c78916ecaf55b1eda4669c6883377bd3
Luiz Gustavo <32493929+luizgustv@users.noreply.github.com>
2021-06-16 21:34:48 -0300

    [FEATURE] implementing login and enable token jwt generate

--- src/main/kotlin/br/com/micronaut/adapter/controller/ClientController.kt
+++ src/main/kotlin/br/com/micronaut/adapter/controller/ClientController.kt
@@ -8,6 +8,9 @@ import br.com.micronaut.application.service.FindDocumentService
 import br.com.micronaut.application.service.RegisterClientService
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule
+import java.security.Principal
 
 @Controller("/micronaut/v1")
 class ClientController(
@@ -15,13 +18,16 @@ class ClientController(
     private val findDocumentService: FindDocumentService
 ) : ClientApi {
 
+    @Secured(SecurityRule.IS_ANONYMOUS)
     override fun testApi(): String = "Hello testing..."
 
+    @Secured(SecurityRule.IS_ANONYMOUS)
     override fun registerClient(request: ClientRequest): HttpResponse<ClientResponse> {
         val response = registerClientService.registerClient(request.toDomain())
         return HttpResponse.ok(ClientResponse(response))
     }
 
+    @Secured(SecurityRule.IS_AUTHENTICATED)
     override fun findDocument(clientId: String): HttpResponse<FindDocumentResponse> {
         val response = findDocumentService.findDocument(clientId)
         return HttpResponse.ok(FindDocumentResponse(response))
