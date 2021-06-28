21eebd9b507c1b453ae5a8e9b0f57a8ed0793c98
Luiz Gustavo <32493929+luizgustv@users.noreply.github.com>
2021-06-28 20:03:09 -0300

    revert changes

--- src/main/kotlin/br/com/micronaut/config/AuthenticationProviderUserPassword.kt
+++ src/main/kotlin/br/com/micronaut/config/AuthenticationProviderUserPassword.kt
@@ -5,22 +5,20 @@ import io.micronaut.security.authentication.*
 import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
 import io.reactivex.FlowableEmitter
-import io.reactivex.Maybe
 import org.reactivestreams.Publisher
+import java.util.*
 import javax.inject.Singleton
-
 
 @Singleton
 class AuthenticationProviderUserPassword() : AuthenticationProvider {
 
     override fun authenticate(
-        httpRequest: HttpRequest<*>?,
-        authenticationRequest: AuthenticationRequest<*, *>
-    ): Publisher<AuthenticationResponse> {
+        httpRequest: HttpRequest<*>?, authenticationRequest: AuthenticationRequest<*, *>): Publisher<AuthenticationResponse> {
 
-        return Flowable.create({ emitter: FlowableEmitter<AuthenticationResponse> ->
+        return Flowable.create(
+            { emitter: FlowableEmitter<AuthenticationResponse> ->
             if (authenticationRequest.identity == "user" && authenticationRequest.secret == "password") {
-                emitter.onNext(UserDetails(authenticationRequest.identity as String, ArrayList()))
+                emitter.onNext(UserDetails(authenticationRequest.identity as String, Collections.emptyList()))
                 emitter.onComplete()
             } else {
                 emitter.onError(AuthenticationException(AuthenticationFailed()))
