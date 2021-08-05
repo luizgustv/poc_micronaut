07033ccc46c6a095b5ed72035d588ba3ddfbeae4
Luiz Gustavo <32493929+luizgustv@users.noreply.github.com>
2021-08-05 20:28:06 -0300

    remove uncomments lines

--- local/localstack/create-resources.sh
+++ local/localstack/create-resources.sh
@@ -2,23 +2,12 @@
 
 LOCALSTACK_HOST=localstack
 
-#waiting_resources() {
-#  resource=$1
-#  host=$2
-#  port=$3
-#  while ! nc -z $host $port; do
-#    echo "[INFO] - Waiting for resources ${resource} in ${host}:${port}..."
-#    sleep 1
-#  done
-#}
-
-waiting_resources "Localstack" $LOCALSTACK_HOST 4566
-
 echo "[INFO] - Waiting for ready resources..."
 sleep 10
 
 echo "[INFO] - Creating dynamodb table:"
 
+#comando para criar a tabela de acordo com o json
 aws dynamodb create-table --endpoint-url http://$LOCALSTACK_HOST:4566 --cli-input-json file://client-table.json
 
 echo "[INFO] - Resources created..."
